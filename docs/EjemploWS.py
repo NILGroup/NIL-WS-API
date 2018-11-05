6291685e71fd32d4d9d1a7ccaac98416d3b615e7
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2018-11-05 16:47:23 +0100

    Ejemplo de Servicio Web NiL con python usable.

--- docs/EjemploWS.py
+++ docs/EjemploWS.py
@@ -1,46 +1,3 @@
-# Buenas prácticas para la API de Servicios Web NiL
-
-Este documento recoge una serie de buenas prácticas para el diseño e
-implementación de servicios web en el grupo NiL. Estas buenas prácticas sirven
-como punto de encuentro, de forma que los servicios que las sigan puedan
-interoperar con facilidad y ser integrados dentro del ecosistema general del
-grupo.
-
-## Entrada/Salida
-
-Lo más importante a la hora de hacer servicios interoperables es la
-entrada/salida de estos servicios. Se seguirán los siguientes puntos:
-
-- La comunicación será mediante HTTP/2 sobre TCP. No se usará HTTPS (o no
-  exclusivamente), pues esto es responsabilidad del gateway que da acceso a los
-  servicios.
-- El cuerpo de los mensajes HTTP, tanto de entrada como de salida, será JSON
-  (`Content-type: application/json; charset=utf-8`). Más concretamente, será un
-  *objeto JSON* con claves correspondientes a los valores de entrada o salida.
-- Los servicios podrán recibir parámetros en la url (bien en el path, o como
-  query parameters), sólo si son simples y singulares. Por ejemplo, un número, o
-  una palabra suelta, pero no una frase o un texto.
-- Se utilizarán los códigos de estado HTTP en la respuesta para indicar el
-  resultado de la comunicación, por ejemplo:
-  - `200`: Se ha podido ejecutar el servicio correctamente.
-  - `400`: Hay algún error en los parámetros.
-  - `404`: El recurso o servicio solicitado no existe.
-  - `500`: El servidor ha tenido un problema inesperado que impide la generación
-    de una respuesta adecuada.
-- Los servicios devolverán siempre, además del código de estado, un objeto JSON
-  con la información del resultado. En caso de error, habrá una clave `error`
-  con un valor de tipo `string` que describe el error que ha ocurrido.
-
-## URLs
-
-*TODO*
-
-## Ejemplo
-
-Ejemplo de servicio web usando `python` y `bottle`. También disponible en
-[EjemploWS.py](EjemploWS.py).
-
-```python
 #!/usr/bin/env python3
 '''Este módulo es un ejemplo de cómo hacer un servicio web python con bottle y
 adaptado al estilo NiL.
@@ -121,4 +78,4 @@ if __name__ == '__main__':
     automáticamente se vuelve a lanzar.'''
     print(__doc__)
     bottle.run(host='localhost', port=3000, reloader=True)
-```
+
